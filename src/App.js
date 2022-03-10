b66b032dce98c3a70e7ca05f5b232e01bcb52d75
amine-elhaddaji <71100133+amine-elhaddaji@users.noreply.github.com>
2022-03-10 11:27:44 +0100

    Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
+import Rubrique from "./component/rubriques";
+import Layout from "./FichiersPartages/Layout";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route component={Rubrique} path="/" exact />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
-export default App;
+export default Layout(App);
