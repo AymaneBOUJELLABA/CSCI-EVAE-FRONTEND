14bd4ce6bfcbd2b2416b1a54b9c9520b16f6e89b
AymaneBOUJELLABA <59460654+AymaneBOUJELLABA@users.noreply.github.com>
2022-03-16 14:32:16 +0100

    Update RubriqueService.Js

--- src/services/RubriqueService.Js
+++ src/services/RubriqueService.Js
@@ -1,55 +1,42 @@
 const API_URL = "http://localhost:8082/api/rubriques";
 
-
-export const getAllRubriques = async () =>
-{
-    
-    const response = fetch(API_URL,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) =>
-      {
-        
-        console.log("Get all rubriques : ", data);
-        return data;
-      })
-      .catch((error) =>
-      {
-          console.error("Erreur get All rubriques", error);
-          return error;
-      });
-
-      return response;
-}
-
-
-export const ajoutRubrique = async (rubrique) =>
-{
-    const response = fetch(API_URL+'/AddOrUpdate',
-    {
-        method: "POST",
-        body : JSON.stringify(rubrique),
-        headers: {
-        "Content-Type": "application/json",
-        },
-    })
+export const getAllRubriques = async () => {
+  const response = fetch(API_URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
     .then((response) => response.json())
-    .then((data) =>
-    {
-    
-        console.log("Ajouter rubrique : ", data);
-        return data;
+    .then((data) => {
+      console.log("Get all rubriques : ", data);
+      return data;
     })
-    .catch((error) =>
-    {
-        console.error("Erreur ajouter rubrique", error);
-        return error;
+    .catch((error) => {
+      console.error("Erreur get All rubriques", error);
+      return error;
     });
 
-    return response;
-}
+  return response;
+};
+
+export const ajoutRubrique = async (rubrique) => {
+  const response = fetch(API_URL + "/AddOrUpdate", {
+    method: "PUT",
+    body: JSON.stringify(rubrique),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Ajouter rubrique : ", data);
+      return data;
+    })
+    .catch((error) => {
+      console.error("Erreur ajouter rubrique", error);
+      return error;
+    });
+
+  return response;
+};
