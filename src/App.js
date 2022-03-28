1f5c364f66fbaf224173d71191bcda3a8b2948cb
amine-elhaddaji <71100133+amine-elhaddaji@users.noreply.github.com>
2022-03-28 14:39:56 +0200

    front pour le questionnaire

--- src/App.js
+++ src/App.js
@@ -4,44 +4,51 @@ import "antd/dist/antd.css";
 import { Alert, Button, Result } from "antd";
 import { Route, Switch } from "react-router-dom";
 
-import Error from './shared/Error';
+import Error from "./shared/Error";
 import EvaluationPage from "./components/Evaluation/EvaluationPage";
-import GetEval from "./components/ues/getEval"
+import GetEval from "./components/ues/getEval";
 import GetUe from "./components/ues/getUe";
 import GetUeCode from "./components/ues/getUeCode";
-import Home from './components/home';
-import Layout from './shared/layout';
+import Home from "./components/home";
+import Layout from "./shared/layout";
 import { Link } from "react-router-dom";
-import React from 'react';
-import Rubriques from './components/rubriques';
+import React from "react";
+import Rubriques from "./components/rubriques";
+import Questionnaire from "./components/Questionnaire";
 
 export const App = () => {
   return (
-      <Layout>
-        <Switch>
-          {/** ADD YOUR COMPONENT ROUTES HERE! */}
-          <Route component={Rubriques} path="/rubriques" exact/>
-          <Route exact path="/UniteEnseignement/Evaluation/:code" component={GetEval} />
-          <Route exact path="/UniteEnseignement/:code" component={GetUeCode} />
-          <Route exact path="/UniteEnseignements" component={GetUe} />
-          <Route component={Home} path="/" exact/>
-          <Route render={()=>{
-            return <>
-            <Link to="/">
-              <Button type="primary">Page d'accueil</Button>
-            </Link>
-            <Alert
-            message={"Page non trouvée"}
-            description="Perdu ? Impossible de trouver la page"
-             /> 
-            </>
-          }} />
-                    
-        </Switch>
-      </Layout>
+    <Layout>
+      <Switch>
+        {/** ADD YOUR COMPONENT ROUTES HERE! */}
+        <Route component={Rubriques} path="/rubriques" exact />
+        <Route component={Questionnaire} path="/questionnaire" exact />
+        <Route
+          exact
+          path="/UniteEnseignement/Evaluation/:code"
+          component={GetEval}
+        />
+        <Route exact path="/UniteEnseignement/:code" component={GetUeCode} />
+        <Route exact path="/UniteEnseignements" component={GetUe} />
+        <Route component={Home} path="/" exact />
+        <Route
+          render={() => {
+            return (
+              <>
+                <Link to="/">
+                  <Button type="primary">Page d'accueil</Button>
+                </Link>
+                <Alert
+                  message={"Page non trouvée"}
+                  description="Perdu ? Impossible de trouver la page"
+                />
+              </>
+            );
+          }}
+        />
+      </Switch>
+    </Layout>
   );
 };
 
-
 export default App;
-
---
+++ src/components/Questionnaire/Question.js
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from "react";
+import { Divider, Row, Col, Tag, Rate } from "antd";
+import "./style.css";
+
+import {
+  RiEmotionUnhappyLine,
+  RiEmotionSadLine,
+  RiEmotionNormalLine,
+  RiEmotionLine,
+  RiEmotionLaughLine,
+} from "react-icons/ri";
+
+const Question = (props) => {
+  const [recap, setRecap] = useState({
+    idEvaluation: props.idEvaluation,
+    idEtudiant: null,
+    rubriques: [
+      {
+        idRubrique: props.idRubrique,
+        designation: props.designation,
+        questions: [],
+      },
+    ],
+  });
+  const starsColor = (value) => {
+    if (value === 1) return "red";
+    else if (value === 2) return "yellow";
+    else if (value === 3) return "orange";
+    else if (value === 4) return "blue";
+    else if (value === 5) return "green";
+    else return "grey";
+  };
+  const customIcons = {
+    1: <RiEmotionUnhappyLine className="emote-unhappy" />,
+    2: <RiEmotionSadLine className="emote-sad" />,
+    3: <RiEmotionNormalLine className="emote-normal" />,
+    4: <RiEmotionLine className="emote-happpy" />,
+    5: <RiEmotionLaughLine className="emote-vey-hapy" />,
+  };
+  useEffect(() => {
+    setRecap({
+      ...recap,
+      rubriques: recap.rubriques.map((e) => ({
+        ...e,
+        idRubrique: props.idRubrique,
+        designation: props.designation,
+      })),
+    });
+  }, [props.idRubrique]);
+
+  const onSetNewResponse = (value, idQuestion, intitule, order) => {
+    const response = {
+      idQuestion: idQuestion,
+      intitule: intitule,
+      order: order,
+      reponse: value,
+    };
+    props._onSetNewResponse({ response, idRubrique: props.idRubrique });
+    setRecap({
+      ...recap,
+      rubriques: [
+        ...recap.rubriques.map((e) => ({
+          ...e,
+          questions: [...e.questions, response],
+        })),
+      ],
+    });
+  };
+  return (
+    <>
+      {props.questions.map((qst, index) => (
+        <div key={`${qst.idQuestion}+${index}`}>
+          <Row justify="center">
+            <Col>{qst.intitule}</Col>
+          </Row>
+          <Row justify="center">
+            <Col className="res-col" span={8}>
+              <Tag className="res-tag" color="red">
+                {qst.qualificatif.minimal}
+              </Tag>
+            </Col>
+            <Col className="res-col" span={8}>
+              <Rate
+                // show numbers instead of Start
+                // character={({ index }) => index + 1}
+                //show emojis insteed of stars
+                character={({ index }) => customIcons[index + 1]}
+                key={props.questions.idQuestion}
+                style={{ fontSize: "29px" }}
+                /* onHoverChange={(value, index) => {
+                  const color = starsColor(value);
+                  setStarColor(color);
+                }} */
+                onChange={(value) => {
+                  onSetNewResponse(
+                    value,
+                    qst.idQuestion,
+                    qst.intitule,
+                    qst.order
+                  );
+                }}
+              />
+            </Col>
+            <Col className="res-col" span={8}>
+              <Tag className="res-tag" color="green">
+                {qst.qualificatif.maximal}
+              </Tag>
+            </Col>
+          </Row>
+
+          <Divider />
+        </div>
+      ))}
+      {/* <Container>
+        <Progress
+          type="circle"
+          style={{ position: "absolute", bottom: "194px", right: "-106px" }}
+          percent={0}
+        />
+      </Container> */}
+    </>
+  );
+};
+
+export default Question;
